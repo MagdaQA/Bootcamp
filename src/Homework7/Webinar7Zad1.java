bda8689bdba483f6a0dabd4445d318938845a753
Magda <magdalenadz@gmail.com>
2021-09-21 21:35:30 +0200

    Web1Zad1

---
+++ src/Homework7/Webinar7Zad1.java
@@ -0,0 +1,23 @@
+package Homework7;
+
+public class Webinar7Zad1 {
+
+    public static void main(String[] args) {
+
+        for (int n = 0; n <= 3; n++) {
+            System.out.println(5);
+        }
+
+    }
+}
+     // jak tę pętlę powiązać z metodą?
+    //public static void printEachNthNumberBelow(int n, int limit) {
+
+// zadanie domowe - zaimplementować poniższą metodę
+// n - ile razy wykonać pętlę (ile razy wydrukować liczbę 'limit')
+// limit - liczba, którą chcemy wydrukować n razy
+// np printEachNthNumberBelow(3, 5)
+// 5
+// 5
+// 5
+
