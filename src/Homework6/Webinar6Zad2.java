801287d7e0304d428d635b89dcf33d70a328cf1a
Magda <magdalenadz@gmail.com>
2021-09-21 20:30:10 +0200

    Web6Zad1 i 2

---
+++ src/Homework6/Webinar6Zad2.java
@@ -0,0 +1,18 @@
+package Homework6;
+
+public class Webinar6Zad2 {
+    public static void main(String[] args) {
+
+        String[] Testuj = new String[] {"t", "e", "s", "t", "u", "j"};
+
+        System.out.printf(Testuj[5]);
+        System.out.printf(Testuj[4]);
+        System.out.printf(Testuj[3]);
+        System.out.printf(Testuj[2]);
+        System.out.printf(Testuj[1]);
+        System.out.printf(Testuj[0]);
+
+    }
+  }
+
+
