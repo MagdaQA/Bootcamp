515707b932aef6e002f7a63d110ed7dce9972c28
Magda <magdalenadz@gmail.com>
2021-09-25 17:55:12 +0200

    Number of index starting from String

---
+++ src/Webinar6Zad3.java
@@ -0,0 +1,23 @@
+package Homework6;
+
+public class Webinar6Zad3 {
+    public static void main(String[] args) {
+
+        int[] tablicaLiczby = new int[] {-8, -9, -3, -2, 2, 5, 7, 4, 8,9};
+
+        System.out.println(tablicaLiczby[0]);
+        System.out.println(tablicaLiczby[1]);
+        System.out.println(tablicaLiczby[2]);
+        System.out.println(tablicaLiczby[3]);
+        System.out.println(tablicaLiczby[4]);
+        System.out.println(tablicaLiczby[5]);
+        System.out.println(tablicaLiczby[6]);
+        System.out.println(tablicaLiczby[7]);
+        System.out.println(tablicaLiczby[8]);
+        System.out.println(tablicaLiczby[9]);
+        System.out.println(tablicaLiczby[10]);
+
+    }
+}
+
+
